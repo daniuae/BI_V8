5987447a5da7f3baabe53ec1b3920682bfc132ae
Dhandapani <104203898+daniuae@users.noreply.github.com>
2025-10-09 16:00:56 +0530

    Update Problems_M1_Sample.sql

--- SQL/Test/Problems_M1_Sample.sql
+++ SQL/Test/Problems_M1_Sample.sql
@@ -12,3 +12,28 @@ INSERT INTO employees VALUES (4, 'Neha', 30, 70000);
 
 ❓Problem
 Show employee name, old salary, and new salary after 10% hike.
+---------------------------------------------------------------
+CREATE TABLE departments (
+  dept_id NUMBER PRIMARY KEY,
+  dept_name VARCHAR2(50)
+);
+
+INSERT INTO departments VALUES (10, 'IT');
+INSERT INTO departments VALUES (20, 'HR');
+INSERT INTO departments VALUES (30, 'Finance');
+COMMIT;
+
+
+❓Problem
+Find average salary per department.
+  ---------------------------------------------------------------
+
+🧩 3️⃣ Highest Paid Employee per Department
+❓Problem
+Show each department’s highest-paid employee.
+
+📊 Output
+EMP_NAME	SALARY	DEPT_NAME
+Sita	65000	HR
+Neha	70000	Finance
+Raj	50000	IT
